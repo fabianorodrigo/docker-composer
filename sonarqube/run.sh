7cc4c5f6ccf56bafd25b087ffc120bf41baffc33
Fabiano Nascimento <fabianorodrigo@gmail.com>
2019-10-13 12:07:03 -0300

    Solucao de contorno para problema ao subir o sonarqube

--- sonarqube/run.sh
+++ sonarqube/run.sh
@@ -1,2 +1,4 @@
 echo "By default you can login as admin with password admin"
-docker run -d --name SonarQube -p 9000:9000 -p 9092:9092 sonarqube
+#docker run -d --name SonarQube -p 9000:9000 -p 9092:9092 sonarqube
+sudo sysctl -w vm.max_map_count=262144
+docker-compose up -d
